c56ade645ac4aed955ab5f9fa891bce460e9ef8a
Fanwei <fanwei_kong@berkeley.edu>
2022-08-24 01:04:32 -0700

    added scripts to process template mesh

---
+++ templates/create_template.py
@@ -0,0 +1,133 @@
+#Copyright (C) 2022 Fanwei Kong, Shawn C. Shadden, University of California, Berkeley
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../external'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
+import vtk
+from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
+import numpy as np
+from utils import natural_sort, dice_score, write_scores
+from vtk_utils.vtk_utils import *
+from utils import *
+import argparse 
+import SimpleITK as sitk
+from pre_process import resample_spacing
+import glob
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seg_fn', default='', help='Name of the segmentation file')
+    parser.add_argument('--if_turn_off_erode', action='store_true', default=False, help='If to turn off eroding the segmentation for better separation')
+    parser.add_argument('--mesh_fn', default='', help='Name of the mesh file')
+    parser.add_argument('--target_node_num', default=1000000, type=int, help='Number of node per class')
+    parser.add_argument('--binary', action='store_true', help='Whether to convert to binary segmentation')
+    parser.add_argument('--ref_im', default=None, help='Might need to resample the segmentation that came out of Slicer')
+    parser.add_argument('--output', help='Filename to write to')
+    return parser.parse_args()
+
+def resample_segmentation(im, seg):
+    # Without this the segmentation spacing will not be correct as Slicer changed it.
+    new_segmentation = sitk.Resample(seg, im.GetSize(),
+                                 sitk.Transform(),
+                                 sitk.sitkNearestNeighbor,
+                                 im.GetOrigin(),
+                                 im.GetSpacing(),
+                                 im.GetDirection(),
+                                 0,
+                                 seg.GetPixelID())
+    return new_segmentation
+
+def convert_to_binary(seg, erode=True):
+    left_id = [1, 2, 3, 6]
+    right_id = [4, 5, 7]
+    filt = sitk.BinaryMedianImageFilter()
+    filt.SetRadius(3)
+    # assemble chambers
+    arr = sitk.GetArrayFromImage(seg)
+    arr_total = np.zeros_like(arr) 
+    for i in [1, 2, 3, 4, 5]:
+        arr_total[arr==i] = 1
+    total = filt.Execute(sitk.GetImageFromArray(arr_total))
+    # erode a bit
+    if erode:
+        print("DEBUG: ERODING ...")
+        ef = sitk.BinaryErodeImageFilter()
+        ef.SetKernelRadius(3)
+        ef.SetBackgroundValue(0)
+        ef.SetForegroundValue(1)
+        total = ef.Execute(total)
+    arr_total = sitk.GetArrayFromImage(total)
+    
+    # assemble Ao-LV, PA-RV
+    arr_aolv = np.zeros_like(arr)
+    arr_parv = np.zeros_like(arr)
+    for i in [3, 6]:
+        arr_aolv[arr==i] = 1
+    for i in [5, 7]:
+        arr_parv[arr==i] = 1
+    aolv = filt.Execute(sitk.GetImageFromArray(arr_aolv))
+    parv = filt.Execute(sitk.GetImageFromArray(arr_parv))
+    arr_aolv = sitk.GetArrayFromImage(aolv)
+    arr_parv = sitk.GetArrayFromImage(parv)
+    
+    arr_total[arr_aolv>0] = 1
+    arr_total[arr_parv>0] = 1
+    
+    # mask the multilabel segmentation
+    arr[arr_total==0] = 0
+    seg_b = sitk.GetImageFromArray(arr_total)
+    seg_b.CopyInformation(seg)
+    seg_m = sitk.GetImageFromArray(arr)
+    seg_m.CopyInformation(seg)
+    return seg_b, seg_m
+
+def create_tmplt(seg, target_num):
+    template = convert_to_surfs(seg, new_spacing=[1., 1., 1.], target_node_num=target_num)
+    template = smooth_polydata(template, 25)
+    return create_tmplt_mesh(template, seg)
+
+def create_tmplt_mesh(mesh, ref):
+    SIZE = (128, 128, 128)
+    img_center = np.array(ref.TransformContinuousIndexToPhysicalPoint(np.array(ref.GetSize())/2.0))
+    ref_r = resample_spacing(ref, template_size=SIZE, order=1)[0]  # numpy array
+    img_center2 = np.array(ref_r.TransformContinuousIndexToPhysicalPoint(np.array(ref_r.GetSize())/2.0))
+    transform = build_transform_matrix(ref_r)
+    mesh  = transform_polydata(mesh, img_center2-img_center, transform, SIZE)
+    return mesh, ref_r[0]
+    
+
+if __name__ == '__main__':
+    args = parse()
+    if args.seg_fn != '':
+        seg = sitk.ReadImage(args.seg_fn)
+        if args.binary:
+            seg_b, seg_m = convert_to_binary(seg, not args.if_turn_off_erode)
+            if args.ref_im is not None:
+                seg_b = resample_segmentation(sitk.ReadImage(args.ref_im), seg_b)
+                seg_m = resample_segmentation(sitk.ReadImage(args.ref_im), seg_m)
+            tmplt_manifold, _ = create_tmplt(seg_b, args.target_node_num*14)
+            write_vtk_polydata(tmplt_manifold, args.output[:-4]+'_manifold.vtp')
+            write_vtk_polydata(tmplt_manifold, args.output[:-4]+'_manifold.obj')
+            tmplt, _ = create_tmplt(seg_m, args.target_node_num)
+        else:
+            if args.ref_im is not None:
+                seg = resample_segmentation(sitk.ReadImage(args.ref_im), seg)
+            tmplt, seg_r = create_tmplt(seg, args.target_node_num)
+            sitk.WriteImage(seg_r, args.output[:-3]+'nii.gz')
+    else:
+        tmplt, _ = create_tmplt_mesh(load_vtk_mesh(args.mesh_fn), sitk.ReadImage(args.ref_im))
+    write_vtk_polydata(tmplt, args.output)
+    write_vtk_polydata(tmplt, args.output[:-3]+'vtp')
+
---
+++ templates/make_mesh_info_dat.py
@@ -0,0 +1,262 @@
+#Copyright (C) 2022 Fanwei Kong, Shawn C. Shadden, University of California, Berkeley
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../external'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
+import networkx as nx
+import numpy as np
+import argparse 
+from datetime import datetime
+import scipy.sparse as sp
+import pickle
+from scipy.sparse.linalg.eigen.arpack import eigsh
+from scipy import sparse
+import vtk
+from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
+from vtk_utils.vtk_utils import *
+from pre_process import *
+import trimesh
+
+def chebyshev_polynomials(adj, k):
+    """Calculate Chebyshev polynomials up to order k. Return a list of sparse matrices (tuple representation)."""
+    print("Calculating Chebyshev polynomials up to order {}...".format(k))
+
+    adj_normalized = normalize_adj(adj)
+    laplacian = sp.eye(adj.shape[0]) - adj_normalized
+    largest_eigval, _ = eigsh(laplacian, 1, which='LM')
+    scaled_laplacian = (2. / largest_eigval[0]) * laplacian - sp.eye(adj.shape[0])
+
+    t_k = list()
+    t_k.append(sp.eye(adj.shape[0]))
+    t_k.append(scaled_laplacian)
+
+    def chebyshev_recurrence(t_k_minus_one, t_k_minus_two, scaled_lap):
+        s_lap = sp.csr_matrix(scaled_lap, copy=True)
+        return 2 * s_lap.dot(t_k_minus_one) - t_k_minus_two
+
+    for i in range(2, k+1):
+        t_k.append(chebyshev_recurrence(t_k[-1], t_k[-2], scaled_laplacian))
+    
+    return sparse_to_tuple(t_k)
+
+def unit(vec):
+    return np.array(vec)/np.linalg.norm(vec)
+
+def find_cap(node_id, mesh, tag_id=1, max_cap_num=1000000, tol = 0.5, side_ring_num=4):
+    print("Finding cap for node {} and assigning tag id {}...".format(node_id, tag_id))
+    try:
+        vtk_arr = mesh.GetCellData().GetArray('Face_ID')
+        face_data = vtk_to_numpy(vtk_arr)
+        vtk_arr_s = mesh.GetCellData().GetArray('Side_ID')
+        side_data = vtk_to_numpy(vtk_arr_s)
+    except:
+        face_data = np.zeros(mesh.GetNumberOfCells())
+        side_data = np.zeros(mesh.GetNumberOfCells())
+    ctr_cells, side_cells, face_cells = [], [], []
+
+    if node_id > -1:
+        pt_norms = get_point_normals(mesh)
+        ctr_nrm = pt_norms[node_id,:]
+        queue = [node_id]
+        visited = []
+        count = 0
+        while len(queue) >0 and count < max_cap_num:
+            count += 1
+            c_arr = vtk.vtkIdList()
+            n = queue.pop(0)
+            visited.append(n)
+            mesh.GetPointCells(n, c_arr)
+            if len(ctr_cells) == 0: #record cells connected to the center
+                ctr_cells = [c_arr.GetId(ci) for ci in range(c_arr.GetNumberOfIds())]
+            for c_id in range(c_arr.GetNumberOfIds()):
+                p_arr = vtk.vtkIdList()
+                mesh.GetCellPoints(c_arr.GetId(c_id), p_arr)
+                found = True
+                for p_id in range(p_arr.GetNumberOfIds()):
+                    PID = p_arr.GetId(p_id)
+                    if np.sum(unit(pt_norms[PID, :]) * unit(ctr_nrm))>tol:
+                        if PID not in visited:
+                            visited.append(PID)
+                            queue.append(PID)
+                    else:
+                        found = False
+                        if c_id not in side_cells:
+                            side_cells.append(c_arr.GetId(c_id))
+                        break
+                if found:
+                    face_cells.append(c_arr.GetId(c_id))
+            #print("DEBUG: count: ", count, face_cells)
+    face_data[face_cells] = tag_id
+    # find rings of cells on the side
+    constraint_set = face_cells
+    add_cells = [q for q in face_cells]
+    for _ in range(side_ring_num):
+        add_cells, constraint_set = find_connected_points(add_cells, mesh, constraint_set)
+        side_cells += add_cells
+    side_data[side_cells] = tag_id
+    vtk_arr = numpy_to_vtk(face_data)
+    vtk_arr.SetName('Face_ID')
+    mesh.GetCellData().AddArray(vtk_arr)
+    vtk_arr_s = numpy_to_vtk(side_data)
+    vtk_arr_s.SetName('Side_ID')
+    mesh.GetCellData().AddArray(vtk_arr_s)
+    if node_id > -1:
+        write_vtk_polydata(mesh, '/Users/fanweikong/Documents/Modeling/HeartDeepFFD/template/test_wh_full_3260/debug.vtp')
+    return mesh, ctr_cells, side_cells, face_data
+
+   
+def cal_lap_index(mesh_neighbor):
+    print("Mesh neighbor: ", mesh_neighbor.shape)
+    max_len = 0
+    for j in mesh_neighbor:
+        if len(j) > max_len:
+            max_len = len(j)
+    print("Max vertex degree is ", max_len)
+    lap_index = np.zeros([mesh_neighbor.shape[0], max_len+2]).astype(np.int32)
+    for i, j in enumerate(mesh_neighbor):
+        lenj = len(j)
+        lap_index[i][0:lenj] = j
+        lap_index[i][lenj:-2] = -1
+        lap_index[i][-2] = i
+        lap_index[i][-1] = lenj
+    return lap_index
+
+def get_face_node_list(mesh, output_mesh=False, target_num=None, cap_list=None):
+    mesh_info = {'node_list': [0], 'face_list': [], 'lap_list': [], 'cap_id_list':[], 'face_side_list':[], 'face_ctr_list':[]}
+    total_node = 0
+    region_ids = np.unique(vtk_to_numpy(mesh.GetPointData().GetArray('RegionId'))).astype(int)
+    if output_mesh:
+        poly = []
+    for index, i in enumerate(region_ids):
+        poly_i = thresholdPolyData(mesh, 'RegionId', (i, i),'point')
+        if target_num is not None:
+            rate = max(0., 1. - float(target_num)/poly_i.GetNumberOfPoints())    
+            poly_i = decimation(poly_i, rate)
+        num_pts = poly_i.GetNumberOfPoints()
+        total_node += poly_i.GetNumberOfPoints()
+        mesh_info['node_list'].append(total_node)
+        cells = vtk_to_numpy(poly_i.GetPolys().GetData()) 
+        cells = cells.reshape(poly_i.GetNumberOfCells(), 4)
+        #print(cells)
+        cells = cells[:,1:]
+        # calculate neighbors using trimesh
+        tri = trimesh.Trimesh(vertices=vtk_to_numpy(poly_i.GetPoints().GetData()), faces=(cells), process=False)
+        mesh_info['lap_list'].append(cal_lap_index(np.array(tri.vertex_neighbors)))
+        mesh_info['face_list'].append(cells)
+        if cap_list is not None:
+            ctr_list = []
+            side_list = []
+            for c_i, c in enumerate(cap_list[index]):
+                poly_i, ctr_i, side_i, cap_id_i = find_cap(c, poly_i, tag_id=c_i+1)
+                ctr_list.append(ctr_i)
+                side_list.append(side_i)
+            mesh_info['cap_id_list'].append(cap_id_i)
+            mesh_info['face_side_list'].append(side_list)
+            mesh_info['face_ctr_list'].append(ctr_list)
+        if output_mesh:
+            poly.append(poly_i)
+    if output_mesh:
+        vtk_poly = appendPolyData(poly)
+        return mesh_info, vtk_poly
+    else:
+        return mesh_info 
+
+def make_dat_bc(mesh_fn, sample_fn, weight_fns, ctrl_fns, write=True, output_path=None, num_mesh=7):
+    template = load_vtk_mesh(mesh_fn)
+    sample_mesh = load_vtk_mesh(sample_fn)
+    ctrl_pts = [load_vtk_mesh(ctrl_fn) for ctrl_fn in ctrl_fns]
+    for i, p in enumerate(ctrl_pts):
+        write_polydata_points(p, ctrl_fns[i][:-3]+'vtp')
+    # Break template mesh into different cardiac structures
+    NUM_MESH = num_mesh
+    # cap dict
+    cap_list = [[-1], [964,1801,1893,3103], [-1], [59, 3220], [-1], [3008], [-1]]
+    cap_list = [[-1],[-1],[-1],[-1],[-1],[-1],[-1]]
+    tmplt_mesh_info, template = get_face_node_list(template, cap_list=cap_list, output_mesh=True)
+    print("NODE_LIST: ", tmplt_mesh_info['node_list'])
+    print("CTR_ID_LIST: ", tmplt_mesh_info['face_ctr_list'])
+    print("SIDE_ID_LIST: ", tmplt_mesh_info['face_side_list'])
+    for i in range(len(tmplt_mesh_info['cap_id_list'])):
+        print("CAP DATA: ", tmplt_mesh_info['cap_id_list'][i].shape)
+        print("FACE DATA: ", tmplt_mesh_info['face_list'][i].shape)
+    sample_mesh_info = get_face_node_list(sample_mesh)
+    coords = vtk_to_numpy(template.GetPoints().GetData())
+    weights = [np.genfromtxt(weight_fn,delimiter=',') for weight_fn in weight_fns]
+
+    sample_pts = vtk_to_numpy(sample_mesh.GetPoints().GetData())
+
+    # find the corresponding id of ctrl pts on sample pts
+    id_ctrl_on_sample_all = []
+    for j in range(len(weights)):
+        # find the corresponding id of ctrl pts on sample pts
+        ctrl_coords = vtk_to_numpy(ctrl_pts[j].GetPoints().GetData())
+        id_ctrl_on_sample = find_point_correspondence(sample_mesh, ctrl_pts[j].GetPoints())
+        sample_pts[id_ctrl_on_sample, :] = ctrl_coords
+        id_ctrl_on_sample_all.append(id_ctrl_on_sample)
+    
+    #----------Not needed if sample mesh and template mesh are the same-----
+    id_mesh_on_sample = []
+    for i in range(NUM_MESH):
+        sample_i = vtk.vtkPolyData()
+        sample_i_pts = vtk.vtkPoints()
+        sample_i_pts.SetData(numpy_to_vtk(sample_pts[sample_mesh_info['node_list'][i]: sample_mesh_info['node_list'][i+1], :]))
+        sample_i.SetPoints(sample_i_pts)
+        tmplt_i = vtk.vtkPolyData()
+        tmplt_i_pts = vtk.vtkPoints()
+        tmplt_i_pts.SetData(numpy_to_vtk(coords[tmplt_mesh_info['node_list'][i]:tmplt_mesh_info['node_list'][i+1],:]))
+        tmplt_i.SetPoints(tmplt_i_pts)
+        id_mesh_on_sample.append(find_point_correspondence(tmplt_i, sample_i_pts))
+    #print(id_mesh_on_sample)
+    #-----------------------------------------------------------------------
+    info = {'sample_coords': sample_pts, 'sample_faces': sample_mesh_info['face_list'], 'sample_node_list': sample_mesh_info['node_list'], 'sample_lap_list': sample_mesh_info['lap_list'], \
+            'lap_list': tmplt_mesh_info['lap_list'], \
+            'struct_node_ids': tmplt_mesh_info['node_list'], 'tmplt_faces': tmplt_mesh_info['face_list'], 'bbw': weights, 'tmplt_coords': coords, \
+            'id_ctrl_on_sample_all': id_ctrl_on_sample_all, 'id_mesh_on_sample': id_mesh_on_sample, \
+            'cap_data':tmplt_mesh_info['cap_id_list'], 'cap_ctr_data': tmplt_mesh_info['face_ctr_list'], 'cap_side_data': tmplt_mesh_info['face_side_list']}
+
+    # build graph for contrl pts
+    sample_faces_total = vtk_to_numpy(sample_mesh.GetPolys().GetData())
+    sample_faces_total = sample_faces_total.reshape(sample_mesh.GetNumberOfCells(), 4)
+    mesh = trimesh.Trimesh(vertices=sample_pts, faces=(sample_faces_total[:,1:]), process=False)
+    info['lap_ids'] = cal_lap_index(mesh.vertex_neighbors)
+    adj_1 = nx.adjacency_matrix(mesh.vertex_adjacency_graph, nodelist=range(len(sample_pts)))
+    cheb_1 = chebyshev_polynomials(adj_1,1)
+    info['support'] = cheb_1
+
+    if write:
+        t = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+        pickle.dump(info, open(os.path.join(output_path, t +"_bbw.dat"),"wb"), protocol=2)
+        template.GetPointData().RemoveArray('BC')
+        sample_mesh.GetPointData().RemoveArray('BC')
+        write_vtk_polydata(template, os.path.join(output_path, t+"_template.vtp"))
+        write_vtk_polydata(sample_mesh, os.path.join(output_path, t+"_sample.vtp"))
+    return info
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--volume_fn', default="", help='Name of the volumetric mesh.')
+    parser.add_argument('--tmplt_fn', help='Name of the template mesh.')
+    parser.add_argument('--sample_fn', help='Name of the sampling mesh.')
+    parser.add_argument('--ctrl_fn', default="", help='Name of the control points')
+    parser.add_argument('--ctrl_fns', nargs='+', default=[], help='Name of the control points')
+    parser.add_argument('--weight_fns', nargs='+', default=[], help='Name of the control points')
+    parser.add_argument('--out_dir', help='Path to the output folder')
+    parser.add_argument('--num_mesh', type=int, help='Number of mesh components')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse()
+    make_dat_bc(args.tmplt_fn, args.sample_fn, args.weight_fns, args.ctrl_fns, write=True, output_path=args.out_dir, num_mesh=args.num_mesh)
+
